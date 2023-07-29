d9a85d9bf481da8a4dcd239c02a8e11e599ab030
Shannon Echegaray <shannonechegaray@gmail.com>
2023-07-28 21:05:32 -0300

    Styles to the Paginate

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
-import { GetServerSideProps, GetStaticProps } from "next";
+import { useContext } from "react";
+import { GetServerSideProps } from "next";
 import dynamic from "next/dynamic";
 import Head from "next/head";
 
@@ -10,6 +11,9 @@ import SearchBar from "@/components/search-bar/search-bar";
 
 // HOOKS
 import useCountry from "@/hooks/useCountry";
+
+// CONTEXT
+import { ThemeContext } from "./_app";
 
 const CountryCard = dynamic(
   () => import("@/components/country-card/country-card"),
@@ -24,17 +28,28 @@ interface ICountries {
 }
 
 export default function Countries({ initCountries }: ICountries) {
-
-  const {showCountries: countries, numberOfPages, setSearch, setPage, actualPage} = useCountry(initCountries);
+  const { theme } = useContext(ThemeContext);
+  const {
+    showCountries: countries,
+    numberOfPages,
+    setSearch,
+    setPage,
+    actualPage,
+  } = useCountry(initCountries);
 
   const handleChangePage = (page: number) => {
     setPage(page);
-  }
+  };
+
+  console.log(actualPage, numberOfPages);
 
   return (
     <>
       <Head>
-        <meta name="description" content="All the countries of the world! Come to see." />
+        <meta
+          name="description"
+          content="All the countries of the world! Come to see."
+        />
         <title>RestCountries - Home</title>
         <link rel="icon" href="/flag-icon.png"></link>
       </Head>
@@ -46,14 +61,39 @@ export default function Countries({ initCountries }: ICountries) {
           ))}
         </div>
         <div className="py-5 flex justify-center gap-4">
-          {Array.from({length: 7}, (_, index) => 
-            actualPage - 2 + index > 0 
-          && actualPage - 2 + index < numberOfPages 
-          && (
-              <button onClick={() => handleChangePage(actualPage - 3 + index)} key={index}>
-                {actualPage - 2 + index}
-              </button>
-            )
+          {actualPage - 2 > 0 && (
+            <button
+              className={`px-3 py-2 shadow-md shadow-slate-900 rounded-md`}
+              onClick={() => handleChangePage(0)}
+            >
+              &lt;&lt;
+            </button>
+          )}
+          {Array.from(
+            { length: 6 },
+            (_, index) =>
+              actualPage - 2 + index > 0 &&
+              actualPage - 2 + index <= numberOfPages && (
+                <button
+                  className={`px-3 py-2 shadow-md shadow-slate-900 rounded-md ${
+                    actualPage - 3 + index === actualPage
+                      ? "bg-slate-600 text-white"
+                      : ""
+                  }`}
+                  onClick={() => handleChangePage(actualPage - 3 + index)}
+                  key={index}
+                >
+                  {actualPage - 2 + index}
+                </button>
+              )
+          )}
+          {actualPage + 2 < numberOfPages && (
+            <button
+              className={`px-3 py-2 shadow-md shadow-slate-900 rounded-md`}
+              onClick={() => handleChangePage(numberOfPages - 1)}
+            >
+              &gt;&gt;
+            </button>
           )}
         </div>
       </div>
@@ -62,16 +102,13 @@ export default function Countries({ initCountries }: ICountries) {
 }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-
   let countries: InitFetch[];
 
-  countries = await (
-      await fetch("https://restcountries.com/v3.1/all")
-      ).json();
-  
+  countries = await (await fetch("https://restcountries.com/v3.1/all")).json();
+
   return {
     props: {
-      initCountries: countries
-    }
-  }
+      initCountries: countries,
+    },
+  };
 };
